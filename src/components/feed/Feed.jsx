0d5e0c9eda9745cc527816ced42c0b5dca9da628
ufukcankurt <ufuk.cnk@gmail.com>
2022-05-10 11:29:23 +0300

    Create | Feed component

---
+++ src/components/feed/Feed.jsx
@@ -0,0 +1,18 @@
+import Post from "../post/Post"
+import "./feed.css"
+
+
+const Feed = () => {
+    return (
+        <div className="FeedContainer">
+            <Post/>
+            <Post/>
+            <Post/>
+            <Post/>
+            <Post/>
+            <Post/>
+        </div>
+    )
+}
+
+export default Feed

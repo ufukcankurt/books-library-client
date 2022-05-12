913d6d0d8af355e1983579a93e2d525a3b727d33
ufukcankurt <ufuk.cnk@gmail.com>
2022-05-12 06:11:25 +0300

    Update | userProfileInfo component

--- src/components/userProfileInfo/UserProfileInfo.jsx
+++ src/components/userProfileInfo/UserProfileInfo.jsx
@@ -1,71 +1,93 @@
-import "./userProfileInfo.css"
+import "./userProfileInfo.css";
 import { Link, useNavigate } from "react-router-dom";
 import {
-    School,
-    Work,
-    LocationCity,
-    Cake,
-    InsertLink,
-    TrackChanges,
-  } from "@material-ui/icons";
+  School,
+  Work,
+  LocationCity,
+  Cake,
+  InsertLink,
+  TrackChanges,
+} from "@material-ui/icons";
 
 const UserProfileInfo = () => {
+  let navigate = useNavigate();
 
-    let navigate = useNavigate();
-
-    return (
-        <div className="UserProfileInfoContainer">
-            <div className="coverPhotoContainer">
-            <img className="coverPhoto" src="/assets/cover.jpg" alt="" />
-            <img src="/assets/profile.jpg" alt="" className="profilePhoto" />
+  return (
+    <div className="UserProfileInfoContainer">
+      <div className="coverPhotoContainer">
+        <img className="coverPhoto" src="/assets/cover.jpg" alt="" />
+        <img src="/assets/profile.jpg" alt="" className="profilePhoto" />
+      </div>
+      <div className="settingsContainer">
+        <div
+          className="settingsButton"
+          onClick={() => {
+            navigate("/login");
+          }}
+        >
+          PROFİLİ DÜZENLE
+        </div>
+      </div>
+      <div className="descriptionContainer">
+        <div className="usernameInfo">
+          <p className="nameText">Ufuk Can Kurt</p>
+          <p className="usernameText">@ufukcankurt</p>
+        </div>
+        <div className="bioInfo">
+          <p className="bioText">
+            • <br />
+            `Okumayı bıraktığın gün sonbahardır, ertesi gün cehaletin kışı
+            başlar...
+            <br />•
+          </p>
+        </div>
+        <div className="userProperty">
+          <div className="education">
+            {" "}
+            <School /> Pamukkale Üniversitesi
           </div>
-          <div className="settingsContainer">
-            <div className="settingsButton" onClick={()=>{navigate("/login")}}>PROFİLİ DÜZENLE</div>
+          <div className="job">
+            <Work /> Software Engineer
           </div>
-          <div className="descriptionContainer">
-            <div className="usernameInfo">
-              <p className="nameText">Ufuk Can Kurt</p>
-              <p className="usernameText">@ufukcankurt</p>
-            </div>
-            <div className="bioInfo">
-              <p className="bioText">
-                • <br />
-                `Okumayı bıraktığın gün sonbahardır, ertesi gün cehaletin kışı
-                başlar...<br />•
-              </p>
-            </div>
-            <div className="userProperty">
-              <div className="education">
-                {" "}
-                <School /> Pamukkale Üniversitesi
-              </div>
-              <div className="job">
-                <Work /> Software Engineer
-              </div>
-              <div className="city">
-                <LocationCity /> Denizli
-              </div>
-              <div className="birthday">
-                <Cake />
-                10.05.2022
-              </div>
-              <div className="website">
-                <InsertLink />
-                <Link to="www.instagram.com/ufuk.books">instagram.com/ufuk.books</Link>
-              </div>
-              <div className="goal">
-                <TrackChanges />
-                2022 okuma hedefi: 14/75
-              </div>
-            </div>
-            <div className="followersInfo">
-                <div className="bookNumbers">158 <span className="followersInfoSpan">Kitap</span></div>
-                <div className="followingNumbers">50 <span className="followersInfoSpan">Takip edilen</span></div>
-                <div className="followerNumbers">780 <span className="followersInfoSpan">Takipçi</span></div>
-            </div>
+          <div className="city">
+            <LocationCity /> Denizli
+          </div>
+          <div className="birthday">
+            <Cake />
+            10.05.2022
+          </div>
+          <div className="website">
+            <InsertLink />
+            <Link to="/www.instagram.com/ufuk.books">
+              instagram.com/ufuk.books
+            </Link>
+          </div>
+          <div className="goal">
+            <TrackChanges />
+            2022 okuma hedefi: 14/75
           </div>
         </div>
-    )
-}
+        <div className="followersInfo">
+          <Link to="/ufukcankurt/books" style={{color:"inherit", textDecoration:"none"}}>
+            <div className="bookNumbers">
+              158 <span className="followersInfoSpan">Kitap</span>
+            </div>
+          </Link>
+          <div className="followingNumbers">
+            50 <span className="followersInfoSpan">Takip edilen</span>
+          </div>
+          <div className="followerNumbers">
+            780 <span className="followersInfoSpan">Takipçi</span>
+          </div>
+          <Link  to="/ufukcankurt/books" style={{color:"inherit", textDecoration:"none"}}>
+          <div className="followerNumbers">
+            <span className="followersInfoSpan">Kitaplık</span>
+          </div>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default UserProfileInfo
+export default UserProfileInfo;

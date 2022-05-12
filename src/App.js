0374125393577af7bb325ef28c03e23bf8e5179e
ufukcankurt <ufuk.cnk@gmail.com>
2022-05-12 06:11:43 +0300

    Add | new routes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
-import Home from "./pages/home/Home"
-import Login from "./pages/login/Login";
-import Register from "./pages/register/Register";
-import Profile from "./pages/profile/Profile";
-
+import "./index.css"
 import {
   BrowserRouter,
   Routes,
   Route,
 } from "react-router-dom";
+import Home from "./pages/home/Home"
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+import Profile from "./pages/profile/Profile";
+import News from "./pages/news/News";
+import UserBooks from "./pages/userBooks/UserBooks";
+
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/login" element={<Login/>}/>
-        <Route path="/register" element={<Register/>}/>
-        <Route path="/profile" element={<Profile/>}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/:userId" element={<Profile />} />
+        <Route path="/news" element={<News />} />
+        <Route path="/:userId/books" element={<UserBooks />} />
+        <Route path="/:userId/books/:shelfId" element={<UserBooks />} />
       </Routes>
     </BrowserRouter>
 
@@ -24,3 +30,14 @@ function App() {
 }
 
 export default App;
+
+
+// const Books = () => {
+//   return (
+//       <div className="BooksContainer">
+//           Books
+//       </div>
+//   )
+// }
+
+// export default Books

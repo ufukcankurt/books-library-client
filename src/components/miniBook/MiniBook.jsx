0fc8d9ac3040704279d06b00103e2abd1c700d0a
ufukcankurt <ufuk.cnk@gmail.com>
2022-05-12 05:55:46 +0300

    Create | MiniBook component

---
+++ src/components/miniBook/MiniBook.jsx
@@ -0,0 +1,11 @@
+import "./miniBook.css"
+
+const MiniBook = () => {
+  return (
+      <div className="miniBookContainer">
+          <img src="/assets/books/book_2.jpg" alt="" />
+      </div>
+  )
+}
+
+export default MiniBook

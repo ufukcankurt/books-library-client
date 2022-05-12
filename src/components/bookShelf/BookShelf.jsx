3bf34c1d876a96abbccff1d9c29c68c1d990f15f
ufukcankurt <ufuk.cnk@gmail.com>
2022-05-12 05:56:28 +0300

    Create | BookShelf component

---
+++ src/components/bookShelf/BookShelf.jsx
@@ -0,0 +1,26 @@
+import "./bookShelf.css";
+import MiniBook from "../miniBook/MiniBook";
+import { Link } from "react-router-dom";
+
+const BookShelf = () => {
+  return (
+    <div className="bookShelfContainer">
+      <div className="bookShelfName">Okuduğu kitaplar</div>
+      <div className="bookShelfInfo">
+        <p className="bookShelfBooksCount">158 Kitap</p>
+        <Link to="/ufukcankurt/books/okuyacaklarım" style={{ textDecoration: "none" }}>
+          <p className="bookShelfMore">Hepsini gör</p>
+        </Link>
+      </div>
+      <div className="bookShelfBooksContent">
+        <MiniBook />
+        <MiniBook />
+        <MiniBook />
+        <MiniBook />
+        <MiniBook />
+      </div>
+    </div>
+  );
+};
+
+export default BookShelf;

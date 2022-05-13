98229975232e64b2407047b8126f85dd197b6e60
ufukcankurt <ufuk.cnk@gmail.com>
2022-05-13 20:54:05 +0300

    Create | new routes

--- src/App.js
+++ src/App.js
@@ -10,6 +10,12 @@ import Register from "./pages/register/Register";
 import Profile from "./pages/profile/Profile";
 import News from "./pages/news/News";
 import UserBooks from "./pages/userBooks/UserBooks";
+import UserShelf from "./pages/userShelf/UserShelf";
+import BookDetail from "./pages/bookDetail/BookDetail";
+import UserFollowers from "./pages/userFollowers/UserFollowers";
+import UserFollowings from "./pages/userFollowings/UserFollowings";
+import NewsDetail from "./pages/newsDetail/NewsDetail";
+import ProfileSettings from "./pages/profileSettings/ProfileSettings";
 
 
 function App() {
@@ -19,10 +25,15 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/profile-settings" element={<ProfileSettings />} />
         <Route path="/:userId" element={<Profile />} />
+        <Route path="/:userId/followers" element={<UserFollowers />} />
+        <Route path="/:userId/followings" element={<UserFollowings />} />
+        <Route path="/:userId/shelf" element={<UserBooks />} />
+        <Route path="/:userId/shelf/:shelfId" element={<UserShelf />} />
+        <Route path="/book/:bookId" element={<BookDetail />} />
         <Route path="/news" element={<News />} />
-        <Route path="/:userId/books" element={<UserBooks />} />
-        <Route path="/:userId/books/:shelfId" element={<UserBooks />} />
+        <Route path="/news/:newsId" element={<NewsDetail />} />
       </Routes>
     </BrowserRouter>
 

1918b2910c9a8d1e2388af46aadda7826e43fd9c
ufukcankurt <ufuk.cnk@gmail.com>
2022-05-13 20:51:12 +0300

    Create | OtherQuotes Component

---
+++ src/components/othersQuotes/OthersQuotes.jsx
@@ -0,0 +1,71 @@
+import "./othersQuotes.css";
+
+const OthersQuotes = () => {
+  return (
+    <div className="othersQuotesContainer">
+      <div className="othersQuotesTopside">
+        <div className="othersQuotesUserImg">
+          <img src="/assets/profile.jpg" alt="" />
+        </div>
+        <div className="othersQuotesUserInfo">
+          <div className="othersQuotesNamesContainer">
+            <div className="othersQuotesNamesContent">
+              <p className="othersQuotesFullname">Ufuk Can Kurt</p>
+              <p className="othersQuotesUsername">@ufukcankurt</p>
+            </div>
+            <p className="othersQuotesSeeAll">Tamamını gör</p>
+          </div>
+          <p className="othersQuotesDate">11.05.2022</p>
+        </div>
+      </div>
+      <div className="othersQuotesText">
+        <p>
+          Johann Wolfgang von Goethe (1749-1832): Alman edebiyatının dünyaca
+          ünlü, en önemli yazarlarındandır. Hukuk eğitimi alan ve resim sanatına
+          da ilgi duyan Goethe, doğa bilimleriyle de uğraşmış, araştırmalar
+          yapmış, yazılar yazmıştır. Dünya görüşünü ve sanat anlayışını
+          aktardığı Şiir ve Hakikat en dikkati çekici eserlerinden biridir.
+          Ayrıca Roma Ağıtları, Faust ve pek çok eseri yayımlandığı dönemde
+          büyük ilgi görmüş, yazarın yüzyıllar süren edebi ününü pekiştirmiştir.
+          1774 yılında yazdığı Genç Werther'in Acıları daha önce şiirleri ve
+          oyunları yayımlanan Goethe'nin ilk romanıdır. Eser büyük bir ilgiyle
+          karşılanmış ve 25 yaşındaki yazara kısa sürede bütün Avrupa'da ün
+          kazandırmıştır. Johann Wolfgang von Goethe (1749-1832): Alman
+          edebiyatının dünyaca ünlü, en önemli yazarlarındandır. Hukuk eğitimi
+          alan ve resim sanatına da ilgi duyan Goethe, doğa bilimleriyle de
+          uğraşmış, araştırmalar yapmış, yazılar yazmıştır. Dünya görüşünü ve
+          sanat anlayışını aktardığı Şiir ve Hakikat en dikkati çekici
+          eserlerinden biridir. Ayrıca Roma Ağıtları, Faust ve pek çok eseri
+          yayımlandığı dönemde büyük ilgi görmüş, yazarın yüzyıllar süren edebi
+          ününü pekiştirmiştir. 1774 yılında yazdığı Genç Werther'in Acıları
+          daha önce şiirleri ve oyunları yayımlanan Goethe'nin ilk romanıdır.
+          Eser büyük bir ilgiyle karşılanmış ve 25 yaşındaki yazara kısa sürede
+          bütün Avrupa'da ün kazandırmıştır.
+          Johann Wolfgang von Goethe (1749-1832): Alman edebiyatının dünyaca
+          ünlü, en önemli yazarlarındandır. Hukuk eğitimi alan ve resim sanatına
+          da ilgi duyan Goethe, doğa bilimleriyle de uğraşmış, araştırmalar
+          yapmış, yazılar yazmıştır. Dünya görüşünü ve sanat anlayışını
+          aktardığı Şiir ve Hakikat en dikkati çekici eserlerinden biridir.
+          Ayrıca Roma Ağıtları, Faust ve pek çok eseri yayımlandığı dönemde
+          büyük ilgi görmüş, yazarın yüzyıllar süren edebi ününü pekiştirmiştir.
+          1774 yılında yazdığı Genç Werther'in Acıları daha önce şiirleri ve
+          oyunları yayımlanan Goethe'nin ilk romanıdır. Eser büyük bir ilgiyle
+          karşılanmış ve 25 yaşındaki yazara kısa sürede bütün Avrupa'da ün
+          kazandırmıştır. Johann Wolfgang von Goethe (1749-1832): Alman
+          edebiyatının dünyaca ünlü, en önemli yazarlarındandır. Hukuk eğitimi
+          alan ve resim sanatına da ilgi duyan Goethe, doğa bilimleriyle de
+          uğraşmış, araştırmalar yapmış, yazılar yazmıştır. Dünya görüşünü ve
+          sanat anlayışını aktardığı Şiir ve Hakikat en dikkati çekici
+          eserlerinden biridir. Ayrıca Roma Ağıtları, Faust ve pek çok eseri
+          yayımlandığı dönemde büyük ilgi görmüş, yazarın yüzyıllar süren edebi
+          ününü pekiştirmiştir. 1774 yılında yazdığı Genç Werther'in Acıları
+          daha önce şiirleri ve oyunları yayımlanan Goethe'nin ilk romanıdır.
+          Eser büyük bir ilgiyle karşılanmış ve 25 yaşındaki yazara kısa sürede
+          bütün Avrupa'da ün kazandırmıştır.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default OthersQuotes;

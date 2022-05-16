9045b8c898918f1a880afab049a998d0f31d91c6
ufukcankurt <ufuk.cnk@gmail.com>
2022-05-16 14:41:49 +0300

    Update | Post Component

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,11 +1,84 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 import "./post.css";
 
-const Post = () => {
+const Post = ({ postDetail }) => {
+  const [postType, setPostType] = useState("summary");
+
+  const TimelineAllPost = () => {
+    return (
+      <p className="postBookTextTimeline">
+        Tüm zamanların en sevilen hikâyelerinden biri olan, kırktan fazla dile
+        çevrilen, Oscar ödüllü bir sinema filmi için temel oluşturan ve yirminci
+        yüzyılın en iyi romanlardan biri seçilen Pulitzer ödüllü Bülbülü
+        Öldürmek, Amerika'nın acımasız bir önyargı ile zehirlenmiş güneyinde
+        geçen, sürükleyici, yürek burkan ve dikkat çekici bir büyüme hikâyesi.
+        Büyüleyici güzellikler ve vahşi eşitsizlikler dünyasında haksız yere
+        korkunç bir suçla suçlanan bir “zenci”yi savunmak için her şeyi riske
+        atan bir adamın hikâyesi çocuk kahramanın gözünden anlatılıyor. Şefkat
+        dolu, dramatik ve düşündürücü Bülbülü Öldürmek okurları insan doğasının
+        köklerine; masumiyet ve deneyime, nezaket ve zulme, sevgi ve nefrete,
+        mizah ve pathosa götürüyor. Harper Lee'nin her zaman basit bir aşk
+        hikâyesi olarak gördüğü romanı bugün Amerikan edebiyatının bir şaheseri
+        olarak kabul ediliyor.
+      </p>
+    );
+  };
+
+  const SinglePostDetail = () => {
+    return (
+      <p className="postBookTextSingle">
+        Tüm zamanların en sevilen hikâyelerinden biri olan, kırktan fazla dile
+        çevrilen, Oscar ödüllü bir sinema filmi için temel oluşturan ve yirminci
+        yüzyılın en iyi romanlardan biri seçilen Pulitzer ödüllü Bülbülü
+        Öldürmek, Amerika'nın acımasız bir önyargı ile zehirlenmiş güneyinde
+        geçen, sürükleyici, yürek burkan ve dikkat çekici bir büyüme hikâyesi.
+        Büyüleyici güzellikler ve vahşi eşitsizlikler dünyasında haksız yere
+        korkunç bir suçla suçlanan bir “zenci”yi savunmak için her şeyi riske
+        atan bir adamın hikâyesi çocuk kahramanın gözünden anlatılıyor. Şefkat
+        dolu, dramatik ve düşündürücü Bülbülü Öldürmek okurları insan doğasının
+        köklerine; masumiyet ve deneyime, nezaket ve zulme, sevgi ve nefrete,
+        mizah ve pathosa götürüyor. Harper Lee'nin her zaman basit bir aşk
+        hikâyesi olarak gördüğü romanı bugün Amerikan edebiyatının bir şaheseri
+        olarak kabul ediliyor.
+        <br/> <br/>
+        Tüm zamanların en sevilen hikâyelerinden biri olan, kırktan fazla dile
+        çevrilen, Oscar ödüllü bir sinema filmi için temel oluşturan ve yirminci
+        yüzyılın en iyi romanlardan biri seçilen Pulitzer ödüllü Bülbülü
+        Öldürmek, Amerika'nın acımasız bir önyargı ile zehirlenmiş güneyinde
+        geçen, sürükleyici, yürek burkan ve dikkat çekici bir büyüme hikâyesi.
+        Büyüleyici güzellikler ve vahşi eşitsizlikler dünyasında haksız yere
+        korkunç bir suçla suçlanan bir “zenci”yi savunmak için her şeyi riske
+        atan bir adamın hikâyesi çocuk kahramanın gözünden anlatılıyor. Şefkat
+        dolu, dramatik ve düşündürücü Bülbülü Öldürmek okurları insan doğasının
+        köklerine; masumiyet ve deneyime, nezaket ve zulme, sevgi ve nefrete,
+        mizah ve pathosa götürüyor. Harper Lee'nin her zaman basit bir aşk
+        hikâyesi olarak gördüğü romanı bugün Amerikan edebiyatının bir şaheseri
+        olarak kabul ediliyor.
+
+        <br/><br/>
+        Tüm zamanların en sevilen hikâyelerinden biri olan, kırktan fazla dile
+        çevrilen, Oscar ödüllü bir sinema filmi için temel oluşturan ve yirminci
+        yüzyılın en iyi romanlardan biri seçilen Pulitzer ödüllü Bülbülü
+        Öldürmek, Amerika'nın acımasız bir önyargı ile zehirlenmiş güneyinde
+        geçen, sürükleyici, yürek burkan ve dikkat çekici bir büyüme hikâyesi.
+        Büyüleyici güzellikler ve vahşi eşitsizlikler dünyasında haksız yere
+        korkunç bir suçla suçlanan bir “zenci”yi savunmak için her şeyi riske
+        atan bir adamın hikâyesi çocuk kahramanın gözünden anlatılıyor. Şefkat
+        dolu, dramatik ve düşündürücü Bülbülü Öldürmek okurları insan doğasının
+        köklerine; masumiyet ve deneyime, nezaket ve zulme, sevgi ve nefrete,
+        mizah ve pathosa götürüyor. Harper Lee'nin her zaman basit bir aşk
+        hikâyesi olarak gördüğü romanı bugün Amerikan edebiyatının bir şaheseri
+        olarak kabul ediliyor.
+
+      </p>
+    );
+  };
+
   return (
     <>
       <Link to="/post/123" style={{ color: "inherit", textDecoration: "none" }}>
-        <div className="PostContainer">
+        <div className="PostContainer" style={postType === "summary" ? {backgroundColor:"rgba(240, 248, 255, 0.4)"} : {backgroundColor:"inherit"} }>
           <div className="postTopside">
             <div className="postUserInfo">
               <Link
@@ -25,8 +98,10 @@ const Post = () => {
               </div>
               <p className="postTimeago">▪ 30d </p>
             </div>
-            <div className="postStatus">Bir kitabı okumaya başladı.</div>
+            <div className="postStatus">{postType === "summary" ? "Bir not ekledi." : "Bir kitabı okumaya başladı."}</div>
           </div>
+          {postType === "summary" ? <TimelineAllPost /> : <></>}
+          {postType === "summary" && postDetail ? <SinglePostDetail /> : <></>}
           <Link
             to="/book/123"
             style={{ color: "inherit", textDecoration: "none" }}

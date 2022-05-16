2565683601b9a77e8be1482f823f75c575b62da6
ufukcankurt <ufuk.cnk@gmail.com>
2022-05-16 14:42:08 +0300

    Update | ReadingGoal Component

--- src/components/readingGoal/ReadingGoal.jsx
+++ src/components/readingGoal/ReadingGoal.jsx
@@ -1,18 +1,37 @@
 import "./readingGoal.css";
 import { CircleProgress } from "react-gradient-progress";
 
-const ReadingGoal = () => {
-  return (
-    <div className="readingGoalContainer">
-      <h2 className="readingGoalTitle">2022 Okuma Hedefi</h2>
-      <div className="readingGoalProgress">
-        <div>
-        <CircleProgress
+const ReadingGoal = ({user}) => {
+
+
+  const UserReadingGoalProgress = () => {
+    return (
+      <CircleProgress
+          percentage={80}
+          strokeWidth={8}
+          width={200}
+          secondaryColor={"gray"}
+        />
+    )
+  }
+
+  const LeftRightSides = () => {
+    return (
+      <CircleProgress
           percentage={20}
           strokeWidth={8}
           width={110}
           secondaryColor={"gray"}
         />
+    )
+  }
+ 
+  return (
+    <div className="readingGoalContainer">
+      <h2 className="readingGoalTitle">2022 Okuma Hedefi</h2>
+      <div className="readingGoalProgress">
+        <div>
+        {user ? <UserReadingGoalProgress/> : <LeftRightSides/>   }
         </div>
         <div className="readingGoalInfo">
             <p className="readingGoalReadedCount">5 kitap</p>

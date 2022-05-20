06165e5a3ed5c44b3ba32d17ba2b23a165a798c6
ufukcankurt <ufuk.cnk@gmail.com>
2022-05-20 20:55:56 +0300

    Create | POST request to update user's datas

--- src/components/loginForm/LoginForm.jsx
+++ src/components/loginForm/LoginForm.jsx
@@ -1,42 +1,45 @@
 import "./loginForm.css";
 import { Link, useNavigate } from "react-router-dom";
-import {useState} from "react"
+import {useContext, useState} from "react"
 import axios from "axios";
+import { AuthContext } from "../../context/authContext/AuthContext";
+import { LoginCall } from "../../context/authContext/apiCalls";
+import { loginFailure, loginStart, loginSuccess } from "../../context/authContext/AuthActions";
 
 const LoginForm = () => {
-
-  let navigate = useNavigate()
-
-  const INITAL = {
-    username:"testuser",
-    password:"12345"
-  }
 
   const [username, setUsername] = useState("")
   const [password, setPassword] = useState("")
 
+  let navigate = useNavigate()
+
+  const {isFetching, error, dispatch} = useContext(AuthContext)
+
   const handleSubmit = async (e) => {
     e.preventDefault();
-
-    if( INITAL.username === username && INITAL.password === password){
-      navigate("/")
-    }else{
-      alert("Yanlış bilgi girdiniz.")
-    }
+    
+    // LoginCall({username, password}, dispatch)
+    
+    dispatch(loginStart());
+    try {
+      const res = await axios.post(
+      "auth/login",
+      {username,password}
+      );
+      console.log("resdata:",res.data);
+      dispatch(loginSuccess(res.data));
+    navigate("/")
+  } catch (error) {
+    dispatch(loginFailure());
+  }
 
     // try {
-    //   const res = await fetch("http://localhost:8000/api/auth/login", {
-    //     method: 'POST',
-    //     headers: {
-    //       'Content-Type': 'application/json'
-    //     },
-    //     body: {username}
-    //   })
-    //   console.log(res);
-      
+    //   await axios.post("http://localhost:8000/api/auth/login", {username,password})
+    //   .then(response => response.status === 200 && navigate("/") )
     // } catch (error) {
-    //   console.log(error)
+    //   console.log(error);
     // }
+   
 
   };
 
@@ -48,6 +51,7 @@ const LoginForm = () => {
       <form onSubmit={handleSubmit}>
         <div className="registerFormTitle">
           <h3>Giriş yap</h3>
+          <h2>{error && error.message}</h2>
           <img src="/assets/sign-up.png" alt="" />
         </div>
 
@@ -72,7 +76,7 @@ const LoginForm = () => {
           onChange={(e)=> setPassword(e.target.value)}
         />
 
-    <button onSubmit={handleSubmit} className="secondary-button" type="submit">Giriş Yap</button>
+    <button onSubmit={handleSubmit} className="secondary-button" type="submit" disabled={isFetching}>Giriş Yap</button>
 
     <div className="haveAccount">
           <p>Hesabın yok mu?</p>

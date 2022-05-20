b42484dfb32a84f9c2dcb84e12e6417319d19b89
ufukcankurt <ufuk.cnk@gmail.com>
2022-05-20 20:53:29 +0300

    Add | AuthContextProvider

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { AuthContextProvider } from './context/authContext/AuthContext';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <AuthContextProvider>
+      <App />
+    </AuthContextProvider>
 );
 

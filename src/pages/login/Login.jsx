749b4996632194cfbb208091aa21f62ff324dd46
ufukcankurt <ufuk.cnk@gmail.com>
2022-05-20 20:57:27 +0300

    Update | Login Page

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,14 +1,16 @@
-import "./login.css"
-import LoginImage from "../../components/loginImage/LoginImage"
-import LoginForm from "../../components/loginForm/LoginForm"
+import "./login.css";
+import LoginImage from "../../components/loginImage/LoginImage";
+import LoginForm from "../../components/loginForm/LoginForm";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
-    return (
-        <div className="loginContainer">
-            <LoginImage/>
-            <LoginForm/>
-        </div>
-    )
-}
+  return (
+    <div className="loginContainer">
+      <LoginImage />
+      <LoginForm />
+    </div>
+  );
+};
 
-export default Login
+export default Login;

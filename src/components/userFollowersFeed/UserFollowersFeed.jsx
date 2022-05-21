e28e4570892fd68639c8613f3ca8309cf68416d5
ufukcankurt <ufuk.cnk@gmail.com>
2022-05-21 09:42:12 +0300

    Create | GET request methods to get data dynamically

--- src/components/userFollowersFeed/UserFollowersFeed.jsx
+++ src/components/userFollowersFeed/UserFollowersFeed.jsx
@@ -1,33 +1,39 @@
-import "./userFollowersFeed.css"
-import UserFollowerOne from "../UserFollowerOne/UserFollowerOne"
+import "./userFollowersFeed.css";
+import UserFollowerOne from "../UserFollowerOne/UserFollowerOne";
 
-const UserFollowersFeed = ({followings}) => {
+const UserFollowersFeed = ({ followings, user }) => {
+  console.log("FEED",user);
+
+  const FollowingsComp = () => {
+    return(
+      <>
+      {user.followings?.map((id,i) => {
+        return <UserFollowerOne key={i} id={id}/>;
+      })}
+      </>
+    )
+  }
+
+  const FollowersComp = () => {
+    return(
+      <>
+      {user.followers?.map((id,i) => {
+        return <UserFollowerOne key={i} id={id}/>;
+      })}
+      </>
+    )
+  }
+
   return (
-      <div className="userFollowersFeedContainer">
-          <h2 className="userFollowersFeedTitle">{followings ? "Takip Edenler" : "Takip Edilenler"}</h2>
-          <UserFollowerOne/>
-          <UserFollowerOne/>
-          <UserFollowerOne/>
-          <UserFollowerOne/>
-          <UserFollowerOne/>
-          <UserFollowerOne/>
-          <UserFollowerOne/>
-          <UserFollowerOne/>
-          <UserFollowerOne/>
-          <UserFollowerOne/>
-          <UserFollowerOne/>
-          <UserFollowerOne/>
-          <UserFollowerOne/>
-          <UserFollowerOne/>
-          <UserFollowerOne/>
-          <UserFollowerOne/>
-          <UserFollowerOne/>
-          <UserFollowerOne/>
-          <UserFollowerOne/>
-          <UserFollowerOne/>
-          <UserFollowerOne/>
-      </div>
-  )
-}
+    <div className="userFollowersFeedContainer">
+      <h2 className="userFollowersFeedTitle">
+        {followings ? "Takip Edilenler" : "Takip Edenler" }
+      </h2>
+      
+      {followings ? <FollowingsComp/> : <FollowersComp/>}
+      
+    </div>
+  );
+};
 
-export default UserFollowersFeed
+export default UserFollowersFeed;

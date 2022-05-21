3a9748e629da2ebd4261d980e593346797dd2e76
ufukcankurt <ufuk.cnk@gmail.com>
2022-05-21 09:38:49 +0300

    Create | Get dynamic datas methods

--- src/pages/userFollowers/UserFollowers.jsx
+++ src/pages/userFollowers/UserFollowers.jsx
@@ -3,16 +3,33 @@ import RightBar from "../../components/rightBar/RightBar";
 import UserFollowersFeed from "../../components/userFollowersFeed/UserFollowersFeed";
 import UserProfileInfo from "../../components/userProfileInfo/UserProfileInfo";
 import "./userFollowers.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
 
 const UserFollowers = () => {
+  const  {username}  = useParams();
+  const [user, setUser] = useState({});
+
+  console.log("USEPARAMS:", username);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const res = await axios.get(`http://localhost:8000/api/users?username=${username}`);
+      setUser(res.data);
+    };
+    fetchUser();
+  }, [username]);
+
+  console.log("ELİMDEKİ USER", user);
   return (
     <>
       <Nav />
       <div className="userFollowersContainer">
         <div className="userFollowersTimeline">
-          <UserProfileInfo />
+          <UserProfileInfo user={user}/>
             
-          <UserFollowersFeed />
+          <UserFollowersFeed user={user}  />
         </div>
         <RightBar profile />
       </div>

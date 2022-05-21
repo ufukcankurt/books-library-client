04361e0f9e592a3a9b7b5462c135cc963d84f72e
ufukcankurt <ufuk.cnk@gmail.com>
2022-05-21 09:37:49 +0300

    Create | PUT request method at ProfileSettingsForm

--- src/components/profileSettingsForm/ProfileSettingsForm.jsx
+++ src/components/profileSettingsForm/ProfileSettingsForm.jsx
@@ -1,44 +1,97 @@
-import "./profileSettingsForm.css"
-import { useState } from "react"
+import "./profileSettingsForm.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import MyDatePickerOwn from "../myDatePickerOwn/MyDatePickerOwn";
+import {
+  Alert,
+  AlertIcon,
+  AlertTitle,
+  AlertDescription,
+} from "@chakra-ui/react";
 
-const ProfileSettingsForm = () => {
+const ProfileSettingsForm = ({ user }) => {
+  console.log("JSON:", user);
+  console.log("id:", user._id);
+  const [isSuccess, setIsSuccess] = useState(null);
 
+  useEffect(() => {});
 
-    const handleSubmit = () => {
-
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.put(
+        `http://localhost:8000/api/users/${user._id}`,
+        formData,
+        {
+          headers: {
+            token: `Bearer ${user.accessToken}`,
+          },
+        }
+      );
+      console.log("RESPONSE", res);
+      user.fullname = formData.full_name;
+      user.education = formData.education;
+      user.job = formData.job;
+      user.city = formData.city;
+      user.website = formData.website;
+      user.desc = formData.bio;
+      user.dob_day = formData.dob_day;
+      user.dob_month = formData.dob_month;
+      user.dob_year = formData.dob_year;
+      user.gender = formData.gender_identity;
+      console.log("PLEASE:", user);
+      localStorage.setItem("user", JSON.stringify(user));
+      res.status === 200 ? setIsSuccess(true) : setIsSuccess(false);
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-    const handleChange = (e) => {
+  const handleChange = (e) => {
+    const value =
+      e.target.type === "checkbox" ? e.target.checked : e.target.value;
+    const name = e.target.name;
 
-        const value = e.target.type === "checkbox" ? e.target.checked : e.target.value
-        const name = e.target.name
-    
-        setFormData((prevState)=> ( {
-          ...prevState,
-          [name]:value
-        }))
-      };
-    
-      const [formData, setFormData] = useState({
-        full_name:"",
-        education:"",
-        job:"",
-        city:"",
-        website:"",
-        bio:"",
-        dob_day:"",
-        dob_month:"",
-        dob_year:"",
-        gender_identity:"",
-      })
-    
-      console.log("Form:", formData);
+    setFormData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  const [formData, setFormData] = useState({
+    full_name: user.fullname,
+    education: user.education,
+    job: user.job,
+    city: user.city,
+    website: user.website,
+    bio: user.desc,
+    dob_day: user.dob_day,
+    dob_month: user.dob_month,
+    dob_year: user.dob_year,
+    gender_identity: user.gender,
+  });
+
+  console.log("Form:", formData);
+
+  const AlertComponent = ({ danger }) => {
+    return (
+      <>
+        <Alert status={danger ? danger : "success"}>
+          <AlertIcon />
+          <AlertTitle></AlertTitle>
+          <AlertDescription>
+          {danger ? "Bir hata ile karşılaştık!" : "Bilgileriniz Başarıyla Güncellendi!"}
+           
+          </AlertDescription>
+        </Alert>
+      </>
+    );
+  };
 
   return (
-      <div className="profileSettingsFormContainer">
-          <h2 className="profileSettingsFormTitle">Profili Düzenle</h2>
-          <form onSubmit={handleSubmit}>
-
+    <div className="profileSettingsFormContainer">
+      <h2 className="profileSettingsFormTitle">Profili Düzenle</h2>
+      <form onSubmit={handleSubmit}>
         <label htmlFor="full_name">İsim ve Soyisim</label>
         <input
           type="text"
@@ -46,7 +99,7 @@ const ProfileSettingsForm = () => {
           name="full_name"
           placeholder="İsim ve Soyisim"
           required={true}
-          value= {formData.full_name}
+          value={formData.full_name}
           onChange={handleChange}
         />
         <label htmlFor="education">Eğitim</label>
@@ -55,7 +108,7 @@ const ProfileSettingsForm = () => {
           id="education"
           name="education"
           placeholder="Eğitim"
-          value= {formData.education}
+          value={formData.education}
           onChange={handleChange}
         />
         <label htmlFor="job">Meslek</label>
@@ -64,7 +117,7 @@ const ProfileSettingsForm = () => {
           id="job"
           name="job"
           placeholder="Meslek"
-          value= {formData.job}
+          value={formData.job}
           onChange={handleChange}
         />
         <label htmlFor="city">Şehir</label>
@@ -73,7 +126,7 @@ const ProfileSettingsForm = () => {
           id="city"
           name="city"
           placeholder="Şehir"
-          value= {formData.city}
+          value={formData.city}
           onChange={handleChange}
         />
         <label htmlFor="website">Web Sitesi</label>
@@ -82,52 +135,23 @@ const ProfileSettingsForm = () => {
           id="website"
           name="website"
           placeholder="Websitesi"
-          value= {formData.website}
+          value={formData.website}
           onChange={handleChange}
         />
         <label htmlFor="bio">Biyografi</label>
         <textarea
-        rows="5"
-        cols="20"
-        name="bio"
-        id="bio"
-        placeholder="Biyografinizde kendinizi anlatıp, sevdiğiniz şeylerden bahsedebilirsiniz. Profilinizde gösterilecektir."
-        value={formData.bio}
-        onChange={handleChange}
-        >
-
-        </textarea>
+          rows="5"
+          cols="20"
+          name="bio"
+          id="bio"
+          placeholder="Biyografinizde kendinizi anlatıp, sevdiğiniz şeylerden bahsedebilirsiniz. Profilinizde gösterilecektir."
+          value={formData.bio}
+          onChange={handleChange}
+        ></textarea>
 
         <label>Doğum Günü</label>
         <div className="multiple-input-container">
-          <input
-            type="number"
-            id="dob_day"
-            name="dob_day"
-            placeholder="GG"
-            required={true}
-            value={formData.dob_day}
-            onChange={handleChange}
-          />
-          <input
-            type="number"
-            id="dob_month"
-            name="dob_month"
-            placeholder="AA"
-            required={true}
-            value={formData.dob_month}
-            onChange={handleChange}
-          />
-          <input
-            type="number"
-            id="dob_year"
-            name="dob_year"
-            placeholder="YYYY"
-            required={true}
-            value={formData.dob_year}
-            onChange={handleChange}
-            maxLength={4}
-          />
+          <MyDatePickerOwn formData={formData} setFormData={setFormData} />
         </div>
 
         <label>Cinsiyet</label>
@@ -153,11 +177,16 @@ const ProfileSettingsForm = () => {
           />
           <label htmlFor="woman-gender-identity">Kadın</label>
         </div>
-        <input className="profileSettingsFormButton" type="submit" value="Bilgileri Güncelle" />
-
+        {isSuccess === true ? <AlertComponent /> : ""}
+        {isSuccess === false ? <AlertComponent danger={"error"} /> : ""}
+        <input
+          className="profileSettingsFormButton"
+          type="submit"
+          value="Bilgileri Güncelle"
+        />
       </form>
-      </div>
-  )
-}
+    </div>
+  );
+};
 
-export default ProfileSettingsForm
+export default ProfileSettingsForm;

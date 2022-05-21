f2e1a1d1ea95c79cbc7fadf88a3eda54941fc2eb
ufukcankurt <ufuk.cnk@gmail.com>
2022-05-21 08:16:18 +0300

    Create | Home Page

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,24 @@
+import "./home.css";
+import Nav from "../../components/nav/Nav";
+import LeftBar from "../../components/leftBar/LeftBar";
+import RightBar from "../../components/rightBar/RightBar";
+import Feed from "../../components/feed/Feed";
+
+
+
+const Home = () => {
+  return (
+    <div>
+      <Nav />
+      <div className="homeContainer">
+        <LeftBar />
+        <div className="homeTimeline">
+            <Feed/>
+        </div>
+        <RightBar />
+      </div>
+    </div>
+  );
+};
+
+export default Home;

d7b5128ae46c3a82765a1c8e94658f15e82b205b
ufukcankurt <ufuk.cnk@gmail.com>
2022-05-23 22:33:43 +0300

    Add | new shortcut path at userFollowers Page

--- src/pages/userFollowers/UserFollowers.jsx
+++ src/pages/userFollowers/UserFollowers.jsx
@@ -8,18 +8,19 @@ import axios from "axios";
 import { useParams } from "react-router-dom";
 
 const UserFollowers = () => {
-  const  {username}  = useParams();
+  const FETCH = process.env.REACT_APP_FETCH_PATH;
+  const { username } = useParams();
   const [user, setUser] = useState({});
 
   console.log("USEPARAMS:", username);
 
   useEffect(() => {
     const fetchUser = async () => {
-      const res = await axios.get(`http://localhost:8000/api/users?username=${username}`);
+      const res = await axios.get(`${FETCH}users?username=${username}`);
       setUser(res.data);
     };
     fetchUser();
-  }, [username]);
+  }, [username, FETCH]);
 
   console.log("ELİMDEKİ USER", user);
   return (
@@ -27,9 +28,9 @@ const UserFollowers = () => {
       <Nav />
       <div className="userFollowersContainer">
         <div className="userFollowersTimeline">
-          <UserProfileInfo user={user}/>
-            
-          <UserFollowersFeed user={user}  />
+          <UserProfileInfo user={user} />
+
+          <UserFollowersFeed user={user} />
         </div>
         <RightBar profile />
       </div>

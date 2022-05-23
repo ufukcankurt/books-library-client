ac84e0c12a1675b08d6739d9dc71543e20917cf5
ufukcankurt <ufuk.cnk@gmail.com>
2022-05-23 08:57:36 +0300

    Update && Add | new dates inputs

--- src/components/bookStatusModal/BookStatusModal.jsx
+++ src/components/bookStatusModal/BookStatusModal.jsx
@@ -1,10 +1,16 @@
 import "./bookStatusModal.css";
 import { Close } from "@material-ui/icons";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import moment from "moment";
 
-const BookStatusModal = ({isClicked, setIsClicked}) => {
+const BookStatusModal = ({ setIsClicked, book, userBook }) => {
+  const PF = process.env.REACT_APP_PUBLIC_FOLDER + "books/";
+
   const [formData, setFormData] = useState({
-    book_status: "",
+    bookStatus: userBook?.bookStatus,
+    bookStart: "",
+    bookEnd: "",
   });
 
   const handleChange = (e) => {
@@ -19,10 +25,23 @@ const BookStatusModal = ({isClicked, setIsClicked}) => {
   };
 
   const handleClick = () => {
-    setIsClicked(false)
-  }
+    setIsClicked(false);
+  };
 
-  console.log("form", formData)
+  const onChangeDate = (e) => {
+    const name = e.target.name;
+    const newDate = moment(new Date(e.target.value)).format("YYYY-MM-DD");
+    // setValue(newDate);
+    console.log(newDate); //value picked from date picker
+    setFormData((prevState) => ({
+      ...prevState,
+      [name]: newDate,
+    }));
+  };
+
+  console.log("userbook", userBook);
+
+  console.log("form", formData);
 
   return (
     <div className="bookStatusModalContainer">
@@ -34,58 +53,84 @@ const BookStatusModal = ({isClicked, setIsClicked}) => {
       </div>
       <div className="bookStatusModalTopside">
         <div className="bookStatusModalImg">
-          <img src="/assets/books/book_2.jpg" alt="" />
+          <img src={`${PF}${book.book_img}`} alt="" />
         </div>
         <div className="bookStatusModalButtonContainer">
           <input
             type="radio"
-            id="book_status_finished"
-            name="book_status"
+            id="bookStatus_finished"
+            name="bookStatus"
             required={true}
             value="finished"
             onChange={handleChange}
-            checked={formData.book_status === "finished"}
+            checked={formData.bookStatus === "finished"}
           />
-          <label htmlFor="book_status_finished">Okudum</label>
+          <label htmlFor="bookStatus_finished">Okudum</label>
           <input
             type="radio"
-            id="book_status_reading"
-            name="book_status"
+            id="bookStatus_reading"
+            name="bookStatus"
             required={true}
             value="reading"
             onChange={handleChange}
-            checked={formData.book_status === "reading"}
+            checked={formData.bookStatus === "reading"}
           />
-          <label htmlFor="book_status_reading">Okuyorum</label>
+          <label htmlFor="bookStatus_reading">Okuyorum</label>
           <input
             type="radio"
-            id="book_status_willread"
-            name="book_status"
+            id="bookStatus_willread"
+            name="bookStatus"
             required={true}
             value="willread"
             onChange={handleChange}
-            checked={formData.book_status === "willread"}
+            checked={formData.bookStatus === "willread"}
           />
-          <label htmlFor="book_status_willread">Okuyacağım</label>
+          <label htmlFor="bookStatus_willread">Okuyacağım</label>
           <input
             type="radio"
-            id="book_status_unfinished"
-            name="book_status"
+            id="bookStatus_unfinished"
+            name="bookStatus"
             required={true}
             value="unfinished"
             onChange={handleChange}
-            checked={formData.book_status === "unfinished"}
+            checked={formData.bookStatus === "unfinished"}
           />
-          <label htmlFor="book_status_unfinished">Yarım bıraktım</label>
+          <label htmlFor="bookStatus_unfinished">Yarım bıraktım</label>
         </div>
       </div>
       <div className="bookStatusModalBookInfo">
-        <p className="bookStatusModalPages">Sayfa: 132</p>
-        <p className="bookStatusModalDate">Tarih: 12.05.2022/20.05.2022</p>
+        <p className="bookStatusModalPages">
+          {" "}
+          {book.book_name} - {book.book_author} - {book.book_page} sayfa{" "}
+        </p>
+        {/* <p className="bookStatusModalDate">Tarih: 12.05.2022/20.05.2022</p> */}
+      </div>
+      <div className="bookStatusModalDates">
+        <div className="bookStatusModalMultipleContainer">
+          <label htmlFor="bookStart">Başlama Tarihi</label>
+          <input
+            id="bookStart"
+            type="date"
+            name="bookStart"
+            value={formData.bookStart}
+            onChange={onChangeDate}
+          />
+        </div>
+        <div className="bookStatusModalMultipleContainer">
+          <label htmlFor="bookStatus_willread">Bitirme Tarihi</label>
+          <input
+            id="bookStatus_unfinished"
+            type="date"
+            name="bookEnd"
+            value={formData.bookEnd}
+            onChange={onChangeDate}
+          />
+        </div>
       </div>
       <div className="bookStatusModalShelf">
         <h2 className="bookStatusModalShelfTitle">BU KİTABI RAFLARINA EKLE</h2>
       </div>
+      <div className="bookStatusModalUpdateButton">Kaydet</div>
     </div>
   );
 };

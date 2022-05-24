04107c0fa4cc1478953969449636fe954f1e2a09
ufukcankurt <ufuk.cnk@gmail.com>
2022-05-24 10:52:49 +0300

    Create | GET request to get user

--- src/pages/userBooks/UserBooks.jsx
+++ src/pages/userBooks/UserBooks.jsx
@@ -3,15 +3,30 @@ import Nav from "../../components/nav/Nav";
 import UserProfileInfo from "../../components/userProfileInfo/UserProfileInfo";
 import RightBar from "../../components/rightBar/RightBar";
 import BooksFeed from "../../components/booksFeed/BooksFeed";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
 
 const UserBooks = () => {
+  const { username } = useParams();
+  const [user, setUser] = useState({});
+  const FETCH = process.env.REACT_APP_FETCH_PATH;
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const res = await axios.get(`${FETCH}users?username=${username}`);
+      setUser(res.data);
+    };
+    fetchUser();
+  }, [username, FETCH]);
+
   return (
     <>
       <Nav />
       <div className="userBooksContainer">
         <div className="userBooksTimeline">
-          <UserProfileInfo />
-          <BooksFeed />
+          <UserProfileInfo user={user} />
+          <BooksFeed user={user} />
         </div>
         <RightBar profile />
       </div>

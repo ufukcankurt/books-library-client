63eb4a0e3527aa8f4be19c566dc58674f444f358
ufukcankurt <ufuk.cnk@gmail.com>
2022-05-24 10:56:56 +0300

    Create | PUT request to create a new shelf

--- src/components/booksFeed/BooksFeed.jsx
+++ src/components/booksFeed/BooksFeed.jsx
@@ -1,36 +1,78 @@
 import "./booksFeed.css";
 import BookShelf from "../bookShelf/BookShelf";
-import { useState } from "react";
+import { useContext, useEffect, useState } from "react";
+import axios from "axios";
+import { AuthContext } from "../../context/authContext/AuthContext";
+import AlertComp from "../alertComp/AlertComp";
 
-const BooksFeed = () => {
+const BooksFeed = ({ user }) => {
+  const FETCH = process.env.REACT_APP_FETCH_PATH;
   const [isClicked, setIsClicked] = useState(false);
   const [newShelf, setNewShelf] = useState("");
+  const [allShelf, setAllShelf] = useState([]);
+  const { user: currentUser, dispatch } = useContext(AuthContext);
+  const [status, setStatus] = useState(2);
+  const successMessage = "Yeni raf başarıyla eklendi.";
+  const errorMessage = "Zaten bu raf mevcut";
+
+  useEffect(() => {
+    setAllShelf(user?.allShelfs);
+  }, [user.allShelfs]);
 
   const handleChange = (e) => {
-    setNewShelf(e.target.value)
+    setNewShelf(e.target.value);
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault()
-
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (allShelf.includes(newShelf)) {
+      return alert(errorMessage);
+    }
+    allShelf.push(newShelf);
     // fetch request
+    try {
+      const res = await axios.put(
+        `${FETCH}users/${currentUser._id}`,
+        { allShelfs: allShelf },
+        {
+          headers: {
+            token: `Bearer ${currentUser.accessToken}`,
+          },
+        }
+      );
+      dispatch({ type: "ADDNEWSHELF", payload: allShelf });
+      setStatus(res.status);
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   const clickedHandleCreateNewShelf = (e) => {
     setIsClicked(!isClicked);
   };
 
+  const CreateNewShefComp = () => {
+    return (
+      <div
+        className="booksFeedNewShelfButton"
+        onClick={clickedHandleCreateNewShelf}
+      >
+        Yeni Raf Ekle
+      </div>
+    );
+  };
+
   return (
     <div className="booksFeedContainer">
+      {status === 200 ? (
+        <AlertComp status="success" message={successMessage} />
+      ) : (
+        ""
+      )}
       <div className="booksFeedTop">
         <div className="booksFeedTopTitles">
           <h2 className="booksFeedTitle">Kitapları</h2>
-          <div
-            className="booksFeedNewShelfButton"
-            onClick={clickedHandleCreateNewShelf}
-          >
-            Yeni Raf Ekle
-          </div>
+          {user._id === currentUser._id ? <CreateNewShefComp /> : ""}
         </div>
         <div
           className="booksFeedNewShelfModal"
@@ -51,10 +93,9 @@ const BooksFeed = () => {
           </div>
         </div>
       </div>
-      <BookShelf />
-      <BookShelf />
-      <BookShelf />
-      <BookShelf />
+      {user.allShelfs?.map((shelf, i) => (
+        <BookShelf key={i} shelf={shelf} user={user} />
+      ))}
     </div>
   );
 };

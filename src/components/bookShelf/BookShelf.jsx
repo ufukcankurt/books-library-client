9a6ffe59174750fff357adadf52d37aa978d5bc1
ufukcankurt <ufuk.cnk@gmail.com>
2022-05-24 10:55:47 +0300

    Create | PUT request to create a new shelf

--- src/components/bookShelf/BookShelf.jsx
+++ src/components/bookShelf/BookShelf.jsx
@@ -1,23 +1,35 @@
 import "./bookShelf.css";
 import MiniBook from "../miniBook/MiniBook";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
-const BookShelf = () => {
+const BookShelf = ({shelf, user}) => {
+  let actualName = ""
+  const [count, setCount] = useState(0)
+  if(shelf === "Okuyacaklarım"){
+     actualName = "willread"
+  }else if(shelf === "Yarım Bıraktıklarım"){
+     actualName = "unfinished"
+  }else if (shelf === "Okuduklarım"){
+     actualName = "finished"
+  }else{
+     actualName = ""
+  }
   return (
     <div className="bookShelfContainer">
-      <div className="bookShelfName">Okuduğu kitaplar</div>
+      <div className="bookShelfName">{shelf}</div>
       <div className="bookShelfInfo">
-        <p className="bookShelfBooksCount">158 Kitap</p>
+        <p className="bookShelfBooksCount">{count} Kitap</p>
         <Link to="/ufukcankurt/shelf/okuyacaklarım" style={{ textDecoration: "none" }}>
           <p className="bookShelfMore">Hepsini gör</p>
         </Link>
       </div>
       <div className="bookShelfBooksContent">
-        <MiniBook />
-        <MiniBook />
-        <MiniBook />
-        <MiniBook />
-        <MiniBook />
+        {
+          user.bookShelf?.map((book,i)=> (
+            (book.bookHasShelf.includes(shelf) || book.bookStatus === actualName ? <MiniBook key={i} bookId={book.bookId} setCount={setCount}/> :"")
+          ))
+        }
       </div>
     </div>
   );

8e7f7d8a54087f702a016b2dc8b975505d16f83e
ufukcankurt <ufuk.cnk@gmail.com>
2022-05-25 21:51:59 +0300

    Update | the data dynamically at CurrentBook

--- src/components/currentBook/CurrentBook.jsx
+++ src/components/currentBook/CurrentBook.jsx
@@ -1,14 +1,22 @@
+import { useEffect } from "react";
 import "./currentBook.css"
+import { Link } from "react-router-dom";
 
-const CurrentBook = () => {
+const CurrentBook = ({book, setReadingCount}) => {
+    const PF = process.env.REACT_APP_PUBLIC_FOLDER + "books/";
+    useEffect(()=> {
+        setReadingCount((prev) => prev + 1)
+    },[])
     return (
         <div className="CurrentBookContainer">
+            <Link to={`/book/${book.bookId}`}>
             <div className="currentBookImg">
-                <img src="./assets/books/bulbulu_oldurmek.jpg" alt="" />
+                <img src={`${PF}${book.bookImg}`} alt="" />
             </div>
+            </Link>
             <div className="currentBookInfo">
-                <p className="currentBookName">Bülbülü Öldürmek</p>
-                <p className="currentBookAuthor">Harper Lee</p>
+                <p className="currentBookName">{book.bookName}</p>
+                <p className="currentBookAuthor">{book.bookAuthor}</p>
             </div>
         </div>
     )

0cfc8e5df4e279bc8f0b4e13449c02fdd11326ac
ufukcankurt <ufuk.cnk@gmail.com>
2022-05-26 20:17:21 +0300

    Create | other users notes at about only one book

--- src/components/othersQuotes/OthersQuotes.jsx
+++ src/components/othersQuotes/OthersQuotes.jsx
@@ -1,68 +1,63 @@
 import "./othersQuotes.css";
+import axios from "axios";
+import { Link, useParams } from "react-router-dom";
+import { useContext, useState, useEffect } from "react";
+import { AuthContext } from "../../context/authContext/AuthContext";
 
-const OthersQuotes = () => {
+const OthersQuotes = ({ note }) => {
+  const FETCH = process.env.REACT_APP_FETCH_PATH;
+  const PF = process.env.REACT_APP_PUBLIC_FOLDER + "books/";
+  const PFPP = process.env.REACT_APP_PUBLIC_FOLDER + "users/";
+  const { user: currentUser } = useContext(AuthContext);
+  const { bookId } = useParams();
+
+  const [book, setBook] = useState({});
+  const [user, setUser] = useState({});
+
+  useEffect(() => {
+    try {
+      const getDatas = async () => {
+        const resBook = await axios.get(`${FETCH}books/${bookId}`, {
+          headers: {
+            token: `Bearer ${currentUser.accessToken}`,
+          },
+        });
+        setBook(resBook.data);
+
+        const resUser = await axios.get(`${FETCH}users?userId=${note.userId}`, {
+          headers: {
+            token: `Bearer ${currentUser.accessToken}`,
+          },
+        });
+        setUser(resUser.data);
+      };
+      getDatas();
+    } catch (error) {
+      console.log(error);
+    }
+  }, [note]);
+
   return (
     <div className="othersQuotesContainer">
       <div className="othersQuotesTopside">
         <div className="othersQuotesUserImg">
-          <img src="/assets/profile.jpg" alt="" />
+          <img src={`${PFPP}${user.profilePicture}`} alt="" />
         </div>
         <div className="othersQuotesUserInfo">
           <div className="othersQuotesNamesContainer">
             <div className="othersQuotesNamesContent">
-              <p className="othersQuotesFullname">Ufuk Can Kurt</p>
-              <p className="othersQuotesUsername">@ufukcankurt</p>
+              <p className="othersQuotesFullname">{user.fullname}</p>
+              <p className="othersQuotesUsername">@{user.username}</p>
             </div>
-            <p className="othersQuotesSeeAll">Tamamını gör</p>
+            <Link to={`/${user.username}/notes/${note._id}`}>
+              <p className="othersQuotesSeeAll">Tamamını gör</p>
+            </Link>
           </div>
-          <p className="othersQuotesDate">11.05.2022</p>
+          <p className="othersQuotesDate">{note.updatedAt?.split("T")[0]}</p>
         </div>
       </div>
       <div className="othersQuotesText">
-        <p>
-          Johann Wolfgang von Goethe (1749-1832): Alman edebiyatının dünyaca
-          ünlü, en önemli yazarlarındandır. Hukuk eğitimi alan ve resim sanatına
-          da ilgi duyan Goethe, doğa bilimleriyle de uğraşmış, araştırmalar
-          yapmış, yazılar yazmıştır. Dünya görüşünü ve sanat anlayışını
-          aktardığı Şiir ve Hakikat en dikkati çekici eserlerinden biridir.
-          Ayrıca Roma Ağıtları, Faust ve pek çok eseri yayımlandığı dönemde
-          büyük ilgi görmüş, yazarın yüzyıllar süren edebi ününü pekiştirmiştir.
-          1774 yılında yazdığı Genç Werther'in Acıları daha önce şiirleri ve
-          oyunları yayımlanan Goethe'nin ilk romanıdır. Eser büyük bir ilgiyle
-          karşılanmış ve 25 yaşındaki yazara kısa sürede bütün Avrupa'da ün
-          kazandırmıştır. Johann Wolfgang von Goethe (1749-1832): Alman
-          edebiyatının dünyaca ünlü, en önemli yazarlarındandır. Hukuk eğitimi
-          alan ve resim sanatına da ilgi duyan Goethe, doğa bilimleriyle de
-          uğraşmış, araştırmalar yapmış, yazılar yazmıştır. Dünya görüşünü ve
-          sanat anlayışını aktardığı Şiir ve Hakikat en dikkati çekici
-          eserlerinden biridir. Ayrıca Roma Ağıtları, Faust ve pek çok eseri
-          yayımlandığı dönemde büyük ilgi görmüş, yazarın yüzyıllar süren edebi
-          ününü pekiştirmiştir. 1774 yılında yazdığı Genç Werther'in Acıları
-          daha önce şiirleri ve oyunları yayımlanan Goethe'nin ilk romanıdır.
-          Eser büyük bir ilgiyle karşılanmış ve 25 yaşındaki yazara kısa sürede
-          bütün Avrupa'da ün kazandırmıştır.
-          Johann Wolfgang von Goethe (1749-1832): Alman edebiyatının dünyaca
-          ünlü, en önemli yazarlarındandır. Hukuk eğitimi alan ve resim sanatına
-          da ilgi duyan Goethe, doğa bilimleriyle de uğraşmış, araştırmalar
-          yapmış, yazılar yazmıştır. Dünya görüşünü ve sanat anlayışını
-          aktardığı Şiir ve Hakikat en dikkati çekici eserlerinden biridir.
-          Ayrıca Roma Ağıtları, Faust ve pek çok eseri yayımlandığı dönemde
-          büyük ilgi görmüş, yazarın yüzyıllar süren edebi ününü pekiştirmiştir.
-          1774 yılında yazdığı Genç Werther'in Acıları daha önce şiirleri ve
-          oyunları yayımlanan Goethe'nin ilk romanıdır. Eser büyük bir ilgiyle
-          karşılanmış ve 25 yaşındaki yazara kısa sürede bütün Avrupa'da ün
-          kazandırmıştır. Johann Wolfgang von Goethe (1749-1832): Alman
-          edebiyatının dünyaca ünlü, en önemli yazarlarındandır. Hukuk eğitimi
-          alan ve resim sanatına da ilgi duyan Goethe, doğa bilimleriyle de
-          uğraşmış, araştırmalar yapmış, yazılar yazmıştır. Dünya görüşünü ve
-          sanat anlayışını aktardığı Şiir ve Hakikat en dikkati çekici
-          eserlerinden biridir. Ayrıca Roma Ağıtları, Faust ve pek çok eseri
-          yayımlandığı dönemde büyük ilgi görmüş, yazarın yüzyıllar süren edebi
-          ününü pekiştirmiştir. 1774 yılında yazdığı Genç Werther'in Acıları
-          daha önce şiirleri ve oyunları yayımlanan Goethe'nin ilk romanıdır.
-          Eser büyük bir ilgiyle karşılanmış ve 25 yaşındaki yazara kısa sürede
-          bütün Avrupa'da ün kazandırmıştır.
-        </p>
+        <p>{note.summary}</p>
       </div>
     </div>
   );

1fc35d6dfb7b0e97f8b2475c1d8827e9ac8ad46c
ufukcankurt <ufuk.cnk@gmail.com>
2022-05-26 19:58:37 +0300

    Create | GET method to get books at BookDetailFeed

--- src/components/bookDetailFeed/BookDetailFeed.jsx
+++ src/components/bookDetailFeed/BookDetailFeed.jsx
@@ -1,15 +1,32 @@
+import "./bookDetailFeed.css";
+import { useParams } from "react-router-dom";
 import BookDetailOneBook from "../bookDetailOneBook/BookDetailOneBook";
 import OthersQuotes from "../othersQuotes/OthersQuotes";
-import "./bookDetailFeed.css";
+import axios from "axios";
+import { AuthContext } from "../../context/authContext/AuthContext";
+import { useContext, useState, useEffect } from "react";
 
 const BookDetailFeed = ({ book, setIsOverlay }) => {
+  const FETCH = process.env.REACT_APP_FETCH_PATH;
+  const { user: currentUser } = useContext(AuthContext);
+  const { bookId } = useParams();
+  const [notes, setNotes] = useState([]);
+
+  useEffect(() => {
+    const getDatas = async () => {
+      const resNote = await axios.get(`${FETCH}notes/book/${bookId}`);
+      setNotes(resNote.data);
+    };
+    getDatas();
+  }, [bookId]);
+
   return (
     <div className="bookDetailFeedContainer">
       <BookDetailOneBook setIsOverlay={setIsOverlay} book={book} />
-      <OthersQuotes />
-      <OthersQuotes />
-      <OthersQuotes />
-      <OthersQuotes />
+
+      {notes?.map((note) => (
+        <OthersQuotes note={note} />
+      ))}
     </div>
   );
 };

20c05655e1307a0c002956a2d68548217154f87e
ufukcankurt <ufuk.cnk@gmail.com>
2022-05-26 20:21:36 +0300

    Create | GET method & Update data dynamically at UserNoteOne

--- src/components/userNoteOne/UserNoteOne.jsx
+++ src/components/userNoteOne/UserNoteOne.jsx
@@ -1,21 +1,48 @@
 import "./userNoteOne.css";
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState, useContext } from "react";
+import { Link, useParams } from "react-router-dom";
+import axios from "axios";
+import { AuthContext } from "../../context/authContext/AuthContext";
 
-function UserNoteOne() {
+function UserNoteOne({ note }) {
+  const FETCH = process.env.REACT_APP_FETCH_PATH;
+  const PF = process.env.REACT_APP_PUBLIC_FOLDER + "books/";
+  const { user: currentUser } = useContext(AuthContext);
+  const { username } = useParams();
+  const [book, setBook] = useState({});
+
+  useEffect(() => {
+    try {
+      const getBook = async () => {
+        const res = await axios.get(`${FETCH}books/${note.bookId}`, {
+          headers: {
+            token: `Bearer ${currentUser.accessToken}`,
+          },
+        });
+        setBook(res.data);
+      };
+      getBook();
+    } catch (error) {
+      console.log(error);
+    }
+  }, [note]);
+
   return (
     <div className="userNoteOneContainer">
       <div className="userNoteOneLeftside">
         <div className="userNoteOneImg">
-          <img src="/assets/books/book_2.jpg" alt="" />
+          <img src={`${PF}${book.book_img}`} alt="" />
         </div>
         <div className="userNoteOneInfo">
-          <p className="userNoteOneBookName">Köpek Kalbi</p>
-          <p className="userNoteOneBookAuthor">Mihail Bulgakov</p>
-          <p className="userNoteOneBookPage">150 Sayfa</p>
+          <p className="userNoteOneBookName">{book.book_name}</p>
+          <p className="userNoteOneBookAuthor">{book.book_author}</p>
+          <p className="userNoteOneBookPage">{book.book_page} Sayfa</p>
         </div>
       </div>
-      <Link to="/ufukcankurt/notes/123" style={{color:"inherit", textDecoration:"none"} }>
+      <Link
+        to={`/${username}/notes/${note._id}`}
+        style={{ color: "inherit", textDecoration: "none" }}
+      >
         <div className="userNoteOneButton">Notu görüntüle</div>
       </Link>
     </div>

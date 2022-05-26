64f7e440b489d68180961ae2e6a68cf261d0328c
ufukcankurt <ufuk.cnk@gmail.com>
2022-05-26 20:02:24 +0300

    Create | new note & get all datas at CreateNoteMain

--- src/components/createNoteMain/CreateNoteMain.jsx
+++ src/components/createNoteMain/CreateNoteMain.jsx
@@ -1,35 +1,93 @@
 import "./createNoteMain.css";
-import { useState } from "react";
+import { useContext, useEffect, useState } from "react";
+import { Link, useNavigate, useParams } from "react-router-dom";
+import axios from "axios";
+import { AuthContext } from "../../context/authContext/AuthContext";
 
-const CreateNoteMain = () => {
+const CreateNoteMain = ({ book, currentBook, currentNote, isExist }) => {
+  const navigate = useNavigate();
+  const PF = process.env.REACT_APP_PUBLIC_FOLDER + "books/";
+  const FETCH = process.env.REACT_APP_FETCH_PATH;
+  const { user: currentUser } = useContext(AuthContext);
+  const { bookId } = useParams();
+
+  useEffect(() => {
+    if (isExist) {
+      setNoteForm({
+        userId: currentUser._id,
+        bookId: bookId,
+        start: 7,
+        character: currentNote?.character,
+        summary: isExist ? currentNote?.summary : "",
+        quote: isExist ? currentNote?.quote : "",
+        private: isExist ? currentNote?.private : "",
+      });
+    }
+  }, [isExist]);
+
   const [noteForm, setNoteForm] = useState({
+    userId: currentUser._id,
+    bookId: bookId,
+    start: 7,
     character: "",
     summary: "",
-    quotes: "",
+    quote: "",
     private: "",
   });
 
   const handleChange = (e) => {
-    setNoteForm((prevState)=> ( {
-        ...prevState,
-        [e.target.name]:e.target.value
-      }))
+    setNoteForm((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
   };
 
-  console.log(noteForm);
-  console.log("charac:", noteForm.character);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      if (isExist) {
+        const res = await axios.put(
+          `${FETCH}notes/${currentNote._id}`,
+          noteForm,
+          {
+            headers: {
+              token: `Bearer ${currentUser.accessToken}`,
+            },
+          }
+        );
+        res.status === 200 &&
+          navigate(`/${currentUser.username}/notes/${currentNote._id}`);
+      } else {
+        const res = await axios.post(`${FETCH}notes/`, noteForm, {
+          headers: {
+            token: `Bearer ${currentUser.accessToken}`,
+          },
+        });
+        res.status === 200 &&
+          navigate(`/${currentUser.username}/notes/${res.data._id}`);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <div className="createNoteMainContainer">
       <div className="createNoteMainBookInfoContent">
-        <div className="createNoteMainBookImg">
-          <img src="/assets/books/book_2.jpg" alt="" />
-        </div>
+        <Link to={`/book/${bookId}`}>
+          <div className="createNoteMainBookImg">
+            <img src={`${PF}${book.book_img}`} alt="" />
+          </div>
+        </Link>
         <div className="createNoteMainBookInfo">
-          <p className="createNoteMainBookName">Köpek Kalbi</p>
-          <p className="createNoteMainBookAuthor">Mihail Bulgakov</p>
-          <p className="createNoteMainBookPage">150 Sayfa</p>
-          <p className="createNoteMainBookDate">20.01.2000</p>
+          <p className="createNoteMainBookName">{book.book_name}</p>
+          <p className="createNoteMainBookAuthor">{book.book_author}</p>
+          <p className="createNoteMainBookPage">{book.book_page} Sayfa</p>
+          {currentBook.bookEnd && currentBook.bookEnd !== "Invalid date" ? (
+            <p className="createNoteMainBookDate">{currentBook.bookEnd}</p>
+          ) : (
+            ""
+          )}
         </div>
       </div>
       <form>
@@ -58,20 +116,22 @@ const CreateNoteMain = () => {
         <div className="createNoteMainQuotes">
           <label htmlFor="quotes">Kitaptan Alıntılar</label>
           <textarea
-            name="quotes"
-            id="quotes"
+            name="quote"
+            id="quote"
             cols="30"
             rows="10"
-            value={noteForm.quotes}
+            value={noteForm.quote}
             onChange={handleChange}
           ></textarea>
         </div>
         <div className="createNoteMainPrivate">
-            <div className="createNoteMainPrivateInfoTextContent">
+          <div className="createNoteMainPrivateInfoTextContent">
             <label htmlFor="private">Kullanıcıya Özel Alan</label>
-            <p className="createNoteMainPrivateInfoText">▪ Bu alana yazdıklarınızı sadece siz görürsünüz.</p>
-            </div>
-          
+            <p className="createNoteMainPrivateInfoText">
+              ▪ Bu alana yazdıklarınızı sadece siz görürsünüz.
+            </p>
+          </div>
+
           <textarea
             name="private"
             id="private"
@@ -81,7 +141,12 @@ const CreateNoteMain = () => {
             onChange={handleChange}
           ></textarea>
         </div>
-        <input className="createNoteMainSubmitButton" type="submit" value="Kaydet" />
+        <input
+          className="createNoteMainSubmitButton"
+          type="submit"
+          value="Kaydet"
+          onClick={handleSubmit}
+        />
       </form>
     </div>
   );

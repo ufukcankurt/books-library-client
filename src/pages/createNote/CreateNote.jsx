06b09260dbb9b4b12d69dc5734f3a5cec37fac96
ufukcankurt <ufuk.cnk@gmail.com>
2022-05-26 20:23:43 +0300

    Update | CreateNote Page

--- src/pages/createNote/CreateNote.jsx
+++ src/pages/createNote/CreateNote.jsx
@@ -2,14 +2,16 @@ import "./createNote.css";
 import Nav from "../../components/nav/Nav";
 import RightBar from "../../components/rightBar/RightBar";
 import CreateNoteFeed from "../../components/createNoteFeed/CreateNoteFeed";
+import CreateNoteSearch from "../../components/createNoteSearch/CreateNoteSearch";
 
 const CreateNote = () => {
   return (
     <>
       <Nav />
       <div className="createNoteContainer">
-        
-        <CreateNoteFeed />
+        <div className="createNoteContent">
+          <CreateNoteSearch />
+        </div>
 
         <RightBar profile />
       </div>

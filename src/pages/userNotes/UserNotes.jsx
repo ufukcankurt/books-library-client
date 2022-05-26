6bf3e02961e7f105e2b22b900b2ba2245648f4d1
ufukcankurt <ufuk.cnk@gmail.com>
2022-05-26 20:26:25 +0300

    Create | GET methods to get User & to get Note at UserNotes

--- src/pages/userNotes/UserNotes.jsx
+++ src/pages/userNotes/UserNotes.jsx
@@ -1,24 +1,50 @@
-import "./userNotes.css"
-import React from 'react'
+import "./userNotes.css";
+import React, { useContext } from "react";
 import Nav from "../../components/nav/Nav";
 import RightBar from "../../components/rightBar/RightBar";
 import UserProfileInfo from "../../components/userProfileInfo/UserProfileInfo";
 import UserNotesFeed from "../../components/userNotesFeed/UserNotesFeed";
-
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
+import { AuthContext } from "../../context/authContext/AuthContext";
 
 function UserNotes() {
+  const FETCH = process.env.REACT_APP_FETCH_PATH;
+  const { user: currentUser } = useContext(AuthContext);
+  const { username } = useParams();
+  const [user, setUser] = useState({});
+  const [notes, setNotes] = useState([]);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const resUser = await axios.get(`${FETCH}users?username=${username}`);
+      setUser(resUser.data);
+      const resNotes = await axios.get(
+        `${FETCH}notes/profile/${currentUser.username}`,
+        {
+          headers: {
+            token: `Bearer ${currentUser.accessToken}`,
+          },
+        }
+      );
+      setNotes(resNotes.data);
+    };
+    fetchUser();
+  }, [username, FETCH]);
+
   return (
     <div>
       <Nav />
       <div className="userNotesContainer">
         <div className="userNotesTimeline">
-          <UserProfileInfo/>
-          <UserNotesFeed/>
+          <UserProfileInfo user={user} />
+          <UserNotesFeed notes={notes} user={user} />
         </div>
-        <RightBar profile/>
+        <RightBar profile />
       </div>
     </div>
-  )
+  );
 }
 
-export default UserNotes
+export default UserNotes;

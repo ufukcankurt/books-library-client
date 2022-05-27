83a476db914bfc61fdf1a8ef84597998fcf0d3ec
ufukcankurt <ufuk.cnk@gmail.com>
2022-05-27 07:36:43 +0300

    Create | GET method to get all news at NewsDetail Page

--- src/pages/newsDetail/NewsDetail.jsx
+++ src/pages/newsDetail/NewsDetail.jsx
@@ -3,15 +3,37 @@ import NewsFeed from "../../components/newsFeed/NewsFeed";
 import NewsPost from "../../components/newsPost/NewsPost";
 import NewsRightBar from "../../components/newsRightBar/NewsRightBar";
 import "./newsDetail.css";
+import { useParams } from "react-router-dom";
+import { useContext, useEffect, useState } from "react";
+import axios from "axios";
+import { AuthContext } from "../../context/authContext/AuthContext";
 
 const NewsDetail = () => {
+  const { user: currentUser } = useContext(AuthContext);
+  const { newsId } = useParams();
+
+  const FETCH = process.env.REACT_APP_FETCH_PATH;
+  const [news, setNews] = useState([]);
+
+  useEffect(() => {
+    const fetchNews = async () => {
+      const res = await axios.get(`${FETCH}news/${newsId}`, {
+        headers: {
+          token: `Bearer ${currentUser.accessToken}`,
+        },
+      });
+      setNews(res.data);
+    };
+    fetchNews();
+  }, [newsId]);
+
   return (
     <>
       <Nav />
       <div className="newsDetailContainer">
         <div className="newsDetailContainer">
           <div className="newsDetailFeedContainer">
-              <NewsPost single/>
+            <NewsPost news={news} single />
           </div>
           <NewsRightBar />
         </div>

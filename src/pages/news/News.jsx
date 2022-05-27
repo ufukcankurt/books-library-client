fbeb937f7a533889f33f52a80f90e80a8a648816
ufukcankurt <ufuk.cnk@gmail.com>
2022-05-27 07:36:14 +0300

    Create | GET method to get all news at News Page

--- src/pages/news/News.jsx
+++ src/pages/news/News.jsx
@@ -1,19 +1,31 @@
-import Nav from "../../components/nav/Nav"
-import NewsFeed from "../../components/newsFeed/NewsFeed"
-import NewsRightBar from "../../components/newsRightBar/NewsRightBar"
-import "./news.css"
+import Nav from "../../components/nav/Nav";
+import NewsFeed from "../../components/newsFeed/NewsFeed";
+import NewsRightBar from "../../components/newsRightBar/NewsRightBar";
+import "./news.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const News = () => {
-    return (
-        <>
-        <Nav/>
-        <div className="newsContainer">
-            
-            <NewsFeed/>
-            <NewsRightBar/>
-        </div>
-        </>
-    )
-}
+  const FETCH = process.env.REACT_APP_FETCH_PATH;
+  const [news, setNews] = useState([]);
 
-export default News
+  useEffect(() => {
+    const fetchNews = async () => {
+      const res = await axios.get(`${FETCH}news/all/news`);
+      setNews(res.data);
+    };
+    fetchNews();
+  }, []);
+
+  return (
+    <>
+      <Nav />
+      <div className="newsContainer">
+        <NewsFeed news={news} />
+        <NewsRightBar />
+      </div>
+    </>
+  );
+};
+
+export default News;

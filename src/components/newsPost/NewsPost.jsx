be7170eb3994535317a249003756e375681a6bbf
ufukcankurt <ufuk.cnk@gmail.com>
2022-05-27 07:34:33 +0300

    Update | Get all datas dynamically

--- src/components/newsPost/NewsPost.jsx
+++ src/components/newsPost/NewsPost.jsx
@@ -1,78 +1,78 @@
 import { Link } from "react-router-dom";
 import "./newsPost.css";
 
-const NewsPost = ({ single }) => {
+const NewsPost = ({ single, news }) => {
+  const PF = process.env.REACT_APP_PUBLIC_FOLDER + "news/";
+
   const SinglePost = () => {
-    return (
-      <p className="newsPostSingleText">
-        2022 Pandemi nedeniyle iki yıldır ertelenen TÜYAP Kitap Fuarı 2022
-        yılında nihayet okurlara kapılarını açacak. TÜYAP Tüm Fuarcılık Yapım
-        A.Ş. tarafından Türkiye Yayıncılar Birliği işbirliğinde hazırlanan 39.
-        Uluslararası İstanbul Kitap Fuarı, 3-11 Aralık 2022 tarihleri arasında
-        TÜYAP Fuar ve Kongre Merkezi-Büyükçekmece’de düzenlenecek. 2022 Pandemi
-        nedeniyle iki yıldır ertelenen TÜYAP Kitap Fuarı 2022 yılında nihayet
-        okurlara kapılarını açacak. TÜYAP Tüm Fuarcılık Yapım A.Ş. tarafından
-        Türkiye Yayıncılar Birliği işbirliğinde hazırlanan 39. Uluslararası
-        İstanbul Kitap Fuarı, 3-11 Aralık 2022 tarihleri arasında TÜYAP Fuar ve
-        Kongre Merkezi-Büyükçekmece’de düzenlenecek. 2022 Pandemi nedeniyle iki
-        yıldır ertelenen TÜYAP Kitap Fuarı 2022 yılında nihayet okurlara
-        kapılarını açacak. TÜYAP Tüm Fuarcılık Yapım A.Ş. tarafından Türkiye
-        Yayıncılar Birliği işbirliğinde hazırlanan 39. Uluslararası İstanbul
-        Kitap Fuarı, 3-11 Aralık 2022 tarihleri arasında TÜYAP Fuar ve Kongre
-        Merkezi-Büyükçekmece’de düzenlenecek. 2022 Pandemi nedeniyle iki yıldır
-        ertelenen TÜYAP Kitap Fuarı 2022 yılında nihayet okurlara kapılarını
-        açacak. TÜYAP Tüm Fuarcılık Yapım A.Ş. tarafından Türkiye Yayıncılar
-        Birliği işbirliğinde hazırlanan 39. Uluslararası İstanbul Kitap Fuarı,
-        3-11 Aralık 2022 tarihleri arasında TÜYAP Fuar ve Kongre
-        Merkezi-Büyükçekmece’de düzenlenecek. 2022 Pandemi nedeniyle iki yıldır
-        ertelenen TÜYAP Kitap Fuarı 2022 yılında nihayet okurlara kapılarını
-        açacak. TÜYAP Tüm Fuarcılık Yapım A.Ş. tarafından Türkiye Yayıncılar
-        Birliği işbirliğinde hazırlanan 39. Uluslararası İstanbul Kitap Fuarı,
-        3-11 Aralık 2022 tarihleri arasında TÜYAP Fuar ve Kongre
-        Merkezi-Büyükçekmece’de düzenlenecek.
-      </p>
-    );
+    return <p className="newsPostSingleText">{news.desc}</p>;
   };
 
   const AllPost = () => {
     return (
       <>
-        <p className="newsPostText">
-          2022 Pandemi nedeniyle iki yıldır ertelenen TÜYAP Kitap Fuarı 2022
-          yılında nihayet okurlara kapılarını açacak. TÜYAP Tüm Fuarcılık Yapım
-          A.Ş. tarafından Türkiye Yayıncılar Birliği işbirliğinde hazırlanan 39.
-          Uluslararası İstanbul Kitap Fuarı, 3-11 Aralık 2022 tarihleri arasında
-          TÜYAP Fuar ve Kongre Merkezi-Büyükçekmece’de düzenlenecek. 2022
-          Pandemi nedeniyle iki yıldır ertelenen TÜYAP Kitap Fuarı 2022 yılında
-          nihayet okurlara kapılarını açacak. TÜYAP Tüm Fuarcılık Yapım A.Ş.
-          tarafından Türkiye Yayıncılar Birliği işbirliğinde hazırlanan 39.
-          Uluslararası İstanbul Kitap Fuarı, 3-11 Aralık 2022 tarihleri arasında
-          TÜYAP Fuar ve Kongre Merkezi-Büyükçekmece’de düzenlenecek.
-        </p>
+        <p className="newsPostText">{news.desc}</p>
+      </>
+    );
+  };
+
+  const SinglePostMid = () => {
+    return (
+      <>
+        <div className="newsPostMid">
+          <img src={`${PF}${news.img}`} alt="" />
+        </div>
+      </>
+    );
+  };
+
+  const AllPostMid = () => {
+    return (
+      <>
+        <Link
+          to={`/news/${news._id}`}
+          style={{ textDecoration: "none", color: "inherit" }}
+        >
+          <div className="newsPostMid">
+            <img src={`${PF}${news.img}`} alt="" />
+          </div>
+        </Link>
+      </>
+    );
+  };
+
+  const SinglePostTop = () => {
+    return (
+      <>
+        <p className="newsPostTitle">{news.title}</p>
+      </>
+    );
+  };
+
+  const AllPostTop = () => {
+    return (
+      <>
+        <Link
+          to={`/news/${news._id}`}
+          style={{ textDecoration: "none", color: "inherit" }}
+        >
+          <p className="newsPostTitle">{news.title}</p>
+        </Link>
       </>
     );
   };
 
   return (
-   
-      <div className="newsPostContainer">
-        <div className="newsPostTopside">
-          <Link to="/news/123" style={{ textDecoration: "none", color: "inherit" }}>
-            <p className="newsPostTitle">
-              17. Ankara Kitap Fuarı ATO Congresium, 18-27 mart 2022
-            </p>
-          </Link>
-          <p className="newsPostTimeago">10.05.2022 - 15.00</p>
-        </div>
-        <Link to="/news/123" style={{ textDecoration: "none", color: "inherit" }}>
-          <div className="newsPostMid">
-            <img src="/assets/news/news_1.jpg" alt="" />
-          </div>
-        </Link>
-        <div className="newsPostBottom">
-          {single ? <SinglePost /> : <AllPost />}
-        </div>
+    <div className="newsPostContainer">
+      <div className="newsPostTopside">
+        {single ? <SinglePostTop /> : <AllPostTop />}
+        <p className="newsPostTimeago">{news.updatedAt?.split("T")[0]}</p>
       </div>
+      {single ? <SinglePostMid /> : <AllPostMid />}
+      <div className="newsPostBottom">
+        {single ? <SinglePost /> : <AllPost />}
+      </div>
+    </div>
   );
 };
 

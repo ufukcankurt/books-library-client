9cf5f64fbf694cf54139d542fbb9b5ed8cef64d9
ufukcankurt <ufuk.cnk@gmail.com>
2022-05-28 11:16:08 +0300

    Create | GET method to get posts by profile or homepage

--- src/components/feed/Feed.jsx
+++ src/components/feed/Feed.jsx
@@ -3,26 +3,36 @@ import "./feed.css";
 import { useContext, useEffect, useState } from "react";
 import axios from "axios";
 import { AuthContext } from "../../context/authContext/AuthContext";
+import Share from "../../components/share/Share";
 
 const Feed = ({ user }) => {
   const FETCH = process.env.REACT_APP_FETCH_PATH;
   const { user: currentUser } = useContext(AuthContext);
   const [posts, setPosts] = useState([]);
+  const [isShare, setIsShare] = useState(false);
 
   useEffect(() => {
-    const fetchPost = async () => {
-      const res = await axios.get(`${FETCH}posts/timeline/${currentUser._id}`);
-      setPosts(
-        res.data.sort((p1, p2) => {
-          return new Date(p2.createdAt) - new Date(p1.createdAt);
-        })
-      );
-    };
-    fetchPost();
-  }, [FETCH, currentUser._id]);
+    try {
+      const fetchPost = async () => {
+        const res = user
+          ? await axios.get(`${FETCH}posts/profile/${user.username}`)
+          : await axios.get(`${FETCH}posts/timeline/${currentUser._id}`);
+        setPosts(
+          res.data.sort((p1, p2) => {
+            return new Date(p2.createdAt) - new Date(p1.createdAt);
+          })
+        );
+      };
+      fetchPost();
+    } catch (error) {
+      console.log(error);
+    }
+  }, [FETCH, currentUser._id, isShare, user]);
 
   return (
     <div className="FeedContainer">
+      {user ? "" : <Share isShare={isShare} setIsShare={setIsShare} />}
+
       {posts?.map((post, i) => (
         <Post key={post._id} post={post} />
       ))}

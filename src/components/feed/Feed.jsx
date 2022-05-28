a1385107d93bc7d55eb9e9c72eb32e4aaafc6649
ufukcankurt <ufuk.cnk@gmail.com>
2022-05-28 06:29:48 +0300

    Create | GET method to get posts at Feed Component

--- src/components/feed/Feed.jsx
+++ src/components/feed/Feed.jsx
@@ -1,18 +1,33 @@
-import Post from "../post/Post"
-import "./feed.css"
+import Post from "../post/Post";
+import "./feed.css";
+import { useContext, useEffect, useState } from "react";
+import axios from "axios";
+import { AuthContext } from "../../context/authContext/AuthContext";
 
+const Feed = ({ user }) => {
+  const FETCH = process.env.REACT_APP_FETCH_PATH;
+  const { user: currentUser } = useContext(AuthContext);
+  const [posts, setPosts] = useState([]);
 
-const Feed = ({user}) => {
-    return (
-        <div className="FeedContainer">
-            <Post/>
-            <Post/>
-            <Post/>
-            <Post/>
-            <Post/>
-            <Post/>
-        </div>
-    )
-}
+  useEffect(() => {
+    const fetchPost = async () => {
+      const res = await axios.get(`${FETCH}posts/timeline/${currentUser._id}`);
+      setPosts(
+        res.data.sort((p1, p2) => {
+          return new Date(p2.createdAt) - new Date(p1.createdAt);
+        })
+      );
+    };
+    fetchPost();
+  }, [FETCH, currentUser._id]);
 
-export default Feed
+  return (
+    <div className="FeedContainer">
+      {posts?.map((post, i) => (
+        <Post key={post._id} post={post} />
+      ))}
+    </div>
+  );
+};
+
+export default Feed;

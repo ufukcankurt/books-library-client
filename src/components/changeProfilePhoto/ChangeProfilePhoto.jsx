26a1375d5103826a274d74c5a2e466402473456d
ufukcankurt <ufuk.cnk@gmail.com>
2022-05-29 08:18:29 +0300

    Update | from now on we can add photos dynamically

--- src/components/changeProfilePhoto/ChangeProfilePhoto.jsx
+++ src/components/changeProfilePhoto/ChangeProfilePhoto.jsx
@@ -1,30 +1,47 @@
 import "./changeProfilePhoto.css";
 import { AddAPhoto } from "@material-ui/icons";
 
-const ChangeProfilePhoto = ({ user }) => {
-  const PF = process.env.REACT_APP_PUBLIC_FOLDER+"users/";
+const ChangeProfilePhoto = ({
+  user,
+  image,
+  setImage,
+  coverImage,
+  setCoverImage,
+}) => {
+  const handleImageChange = (e) => {
+    if (e.target.files[0]) {
+      setImage(e.target.files[0]);
+    }
+  };
+
+  const handleCoverImageChange = (e) => {
+    if (e.target.files[0]) {
+      setCoverImage(e.target.files[0]);
+    }
+  };
+
   return (
     <div className="changeProfilePhotoContainer">
       <div className="changeProfilePhotoCoverBtn">
-        <AddAPhoto />
+        <label id="" htmlFor="upload-cover">
+          <AddAPhoto />
+        </label>
+        <input
+          onChange={handleCoverImageChange}
+          id="upload-cover"
+          type="file"
+        />
       </div>
       <div className="changeProfilePhotoCover">
-        <img
-          src={
-            user.coverPicture ? PF + user.coverPicture : PF + "noCover.jpg"
-          }
-          alt=""
-        />
+        <img src={user.coverPicture} alt="" />
       </div>
       <div className="changeProfilePhotoMain">
-        <img
-          src={
-            user.profilePicture ? PF + user.profilePicture : PF + "noAvatar.png"
-          }
-          alt=""
-        />
+        <img src={user.profilePicture} alt="" />
         <div className="changeProfilePhotoMainBtn">
-          <AddAPhoto />
+          <label id="" htmlFor="upload-pp">
+            <AddAPhoto />
+          </label>
+          <input onChange={handleImageChange} id="upload-pp" type="file" />
         </div>
       </div>
     </div>

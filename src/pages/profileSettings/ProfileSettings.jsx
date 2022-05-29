07f893585efddab8432c4dd1c7f767296b54d383
ufukcankurt <ufuk.cnk@gmail.com>
2022-05-29 08:25:07 +0300

    Create | some datas & send them as props at ProfileSettings

--- src/pages/profileSettings/ProfileSettings.jsx
+++ src/pages/profileSettings/ProfileSettings.jsx
@@ -3,30 +3,33 @@ import Nav from "../../components/nav/Nav";
 import RightBar from "../../components/rightBar/RightBar";
 import ChangeProfilePhoto from "../../components/changeProfilePhoto/ChangeProfilePhoto";
 import ProfileSettingsForm from "../../components/profileSettingsForm/ProfileSettingsForm";
-import { useNavigate, useParams } from "react-router-dom";
-import { useEffect, useState, useContext } from "react";
-import axios from "axios";
+import { useState, useContext } from "react";
 import { AuthContext } from "../../context/authContext/AuthContext";
 
 const ProfileSettings = () => {
-  let { userId } = useParams();
-  let navigate = useNavigate();
-  const {user} = useContext(AuthContext);
-
-  console.log("PROF SETTİNG user:", user);
-  useEffect(() => {
-    if (userId !== user._id) {
-      // navigate("/");
-    }
-  }, []);
+  const { user } = useContext(AuthContext);
+  const [image, setImage] = useState(null);
+  const [coverImage, setCoverImage] = useState(null);
 
   return (
     <div>
       <Nav />
       <div className="profileSettingsContainer">
         <div className="profileSettingsTimeline">
-          <ChangeProfilePhoto user={user}  />
-          <ProfileSettingsForm user={user} />
+          <ChangeProfilePhoto
+            coverImage={coverImage}
+            setCoverImage={setCoverImage}
+            image={image}
+            setImage={setImage}
+            user={user}
+          />
+          <ProfileSettingsForm
+            coverImage={coverImage}
+            setCoverImage={setCoverImage}
+            image={image}
+            setImage={setImage}
+            user={user}
+          />
         </div>
         <RightBar profile />
       </div>

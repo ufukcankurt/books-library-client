c71df6b11e931a33bfb7b86614d9277e117f1d4f
ufukcankurt <ufuk.cnk@gmail.com>
2022-07-20 14:25:10 +0300

    Create | new booksPost Component

---
+++ src/components/booksPost/BooksPost.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./booksPost.css";
+
+const BooksPost = ({ book }) => {
+  const PF = process.env.REACT_APP_PUBLIC_FOLDER + "books/";
+  return (
+    <div className="bookPostContainer">
+      <Link to={`/book/${book._id}`}>
+        <div className="bookPostImg">
+          <img src={`${PF}${book.book_img}`} alt="" />
+        </div>
+      </Link>
+      <div className="bookPostInfos">
+        <p className="bookPostName">{`${book.book_name} - ${book.book_author}`}</p>
+        <p className="bookPostPage">
+          <span className="bookPostBold">Sayfa: </span>
+          {book.book_page}
+        </p>
+        <p className="bookPostPublishingHouse">
+          <span className="bookPostBold">Yayınevi: </span>
+          {book.book_publishingHouse}
+        </p>
+        <p className="bookPostGenre">
+          <span className="bookPostBold">Tür: </span>
+          {book.book_genre.map((genre) => `${genre} - `)}
+        </p>
+        <p className="bookPostIsbn">
+          <span className="bookPostBold">ISBN: </span>
+          {book.book_isbn}
+        </p>
+      </div>
+      <div className="bookPostButton">
+        <Link to={`/book/${book._id}`}>
+          <button>Kitaba Git</button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default BooksPost;

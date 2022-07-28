fdac87326c0f50f8bb2987a72c9f446f522325ef
ufukcankurt <ufuk.cnk@gmail.com>
2022-07-28 12:22:00 +0300

    Create | adminPanelCreate Page

---
+++ src/pages/adminPanelCreate/AdminPanelCreate.jsx
@@ -0,0 +1,32 @@
+import "./adminPanelCreate.css"
+import React, {useContext} from "react";
+import { useParams } from "react-router-dom";
+import Nav from "../../components/nav/Nav";
+import AdminRightBar from "../../components/AdminRightBar/AdminRightBar";
+import AdminCreateBook from "../../components/adminCreateBook/AdminCreateBook";
+import { AuthContext } from "../../context/authContext/AuthContext";
+import AdminCreateAuthor from "../../components/adminCreateAuthor/AdminCreateAuthor";
+import AdminCreateNews from "../../components/adminCreateNews/AdminCreateNews";
+import AdminCreateTodayInHistory from "../../components/adminCreateTodayInHistory/AdminCreateTodayInHistory";
+
+const AdminPanelCreate = () => {
+  const { user: currentUser } = useContext(AuthContext);
+  const { categoryName } = useParams();
+
+  return (
+    <>
+      <Nav />
+      <div className="adminPanelCreateContainer">
+        <div className="adminPanelCreateFeed">
+            {categoryName === "book" ? <AdminCreateBook currentUser={currentUser} /> : ""}
+            {categoryName === "author" ? <AdminCreateAuthor currentUser={currentUser} /> : ""}
+            {categoryName === "news" ? <AdminCreateNews currentUser={currentUser} /> : ""}
+            {categoryName === "todayinhistory" ? <AdminCreateTodayInHistory currentUser={currentUser} /> : ""}
+        </div>
+        <AdminRightBar/>
+      </div>
+    </>
+  );
+};
+
+export default AdminPanelCreate;

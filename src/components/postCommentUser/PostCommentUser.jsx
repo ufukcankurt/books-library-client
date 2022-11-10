a45e5fa5881c23a151d166995250d0ea1434b12a
ufukcankurt <ufuk.cnk@gmail.com>
2022-11-10 21:04:57 +0300

    feat(postcommentuser): create single comment component inside comment section

---
+++ src/components/postCommentUser/PostCommentUser.jsx
@@ -0,0 +1,56 @@
+import "./postCommentUser.css"
+import React, { useContext, useEffect, useState } from 'react'
+import { Link } from "react-router-dom"
+import DeleteIcon from '@mui/icons-material/Delete';
+import { AuthContext } from "../../context/authContext/AuthContext";
+import axios from "axios";
+import TimeAgo from "timeago-react";
+import tr from "timeago.js/lib/lang/tr";
+import * as timeago from "timeago.js";
+
+const PostCommentUser = ({ comment, post, setPost }) => {
+
+    const FETCH = process.env.REACT_APP_FETCH_PATH;
+    const { user: currentUser } = useContext(AuthContext);
+    timeago.register("tr", tr);
+
+
+    const handleRemoveComment = async (e) => {
+        e.preventDefault()
+        const res = await axios.delete(`${FETCH}comments/${comment.comment._id}`, {
+            headers: {
+                token: `Bearer ${currentUser.accessToken}`,
+            }
+        })
+        res.status === 200 && setPost({ ...post, comments: post.comments.filter(c => c.comment._id !== comment.comment._id) })
+    }
+
+    return (
+        <div className="postCommentUserContainer">
+            <Link to={`/${comment.user.username}`}>
+                <div className="postCommentUserImg">
+                    <img src={comment.user.profilePicture} alt="" />
+                </div>
+            </Link>
+            <div className="postCommentUserDetailContent">
+                <Link to={`/${comment.user.username}`}>
+                    <div className="postCommentUserDetail">
+                        <p className="postCommentUserFullname">{comment.user.fullname}</p>
+                        <p className="postCommentUsername">@{comment.user.username}</p>
+                        <p className="postCommentTimeAgo">▪ <TimeAgo datetime={comment.comment.createdAt} locale="tr" /></p>
+                    </div>
+                </Link>
+                <div className="postCommentUserText">
+                    <p className="postCommentUserTextDetail">{comment.comment.desc}</p>
+                </div>
+            </div>
+            {((currentUser._id === comment.comment.userId) || (post.user._id === currentUser._id)) &&
+                <div className="postCommentUserRemove" onClick={handleRemoveComment}>
+                    <DeleteIcon />
+                </div>
+            }
+        </div >
+    )
+}
+
+export default PostCommentUser
